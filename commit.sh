843678263f41ba2a217141b11c730ea13ad85fdf
StarunneR <cloudpool2017@outlook.com>
2020-10-07 21:56:57 +0800

    auto

---
+++ commit.sh
@@ -0,0 +1,4 @@
+git add . #跟踪所有文件，除.gitignore文件规定的
+git commit -m "auto" #暂存文件，暂存附加信息为COMMENT
+git push gitlab master #推送到远端名gitlab的master分支
+read -n 1 #暂停运行
--- upload.sh
+++
@@ -1,4 +0,0 @@
-git add .
-git commit -m "auto"
-git push gitlab master
-read -n 1
