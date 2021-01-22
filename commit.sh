48be89d9160d771fbfd0d573ee88ca854d4a5054
StarunneR <cloudpool2017@outlook.com>
2021-01-23 00:55:23 +0800

    auto

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 git add . #跟踪所有文件，除.gitignore文件规定的
 git commit -m "auto" #暂存文件，暂存附加信息为COMMENT
-git push gitlab master #推送到远端名gitlab的master分支
 hexo clean
-hexo g -d
+hexo g -d #渲染并推送到远端名gitlab的master分支
+git push gitlab master #推送到远端名gitlab的master分支
 read -n 1 #暂停运行
